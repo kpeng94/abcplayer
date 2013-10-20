4240e04a4a61ad75825064f28739848c7253673f
Kulpreet Chilana <contact@kulpreet.com>
2013-10-19 23:47:19 -0400

    Set up lexer test class.

---
+++ src/grammar/LexerTest.java
@@ -0,0 +1,38 @@
+package grammar;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.junit.Test;
+
+/**
+ * This is the test suite for Lexer. These tests will make 
+ * sure your Lexer works as expected.
+ */
+public class LexerTest {
+
+    @Test
+    public void test() {
+      //Testing that the lexer parses a single token
+        verifyLexer("m", new String[] {"m"});
+    }
+    
+    public void verifyLexer(String input, String[] expectedTokens) {
+        CharStream stream = new ANTLRInputStream(input);
+        ABCMusicLexer lexer = new ABCMusicLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        List<? extends Token> actualTokens = lexer.getAllTokens();
+
+        assertEquals(expectedTokens.length, actualTokens.size());
+        
+        for(int i = 0; i < actualTokens.size(); i++) {
+             String actualToken = actualTokens.get(i).getText();
+             String expectedToken = expectedTokens[i];
+             assertEquals(actualToken, expectedToken);
+        }
+    }
+}
