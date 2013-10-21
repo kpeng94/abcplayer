e69fa01ea54623d3394802b311906b9ea4dd2b54
Kevin Peng <kpeng94@mit.edu>
2013-10-21 03:30:55 -0400

    added a lexer test

--- src/grammar/LexerTest.java
+++ src/grammar/LexerTest.java
@@ -58,6 +58,23 @@ public class LexerTest {
     public void testMeasureNote() {
     	verifyLexer("C C C3/4 D/4 E |", new String[] {"C", "C", "C3/4", "D/4", "E", "|"});
     }
+    
+    @Test
+    public void testRandom() {
+        String piece = "X: 1\nT:Piece No.1\nM:4/4\nL:1/4\nQ:1/4=140\nK:C\nC C C3/4 D/4 E | E3/4 D/4 E3/4 F/4 G2 | (3c/2c/2c/2 (3G/2G/2G/2 (3B/2B/2B/2 (3C/2C/2C/2 | G3/4 F/4 E3/4 D/4 C2";       
+        CharStream stream = new ANTLRInputStream(piece);
+        ABCMusicLexer lexer = new ABCMusicLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        List<? extends Token> actualTokens = lexer.getAllTokens();
+
+//        assertEquals(expectedTokens.length, actualTokens.size());
+        
+        for(int i = 0; i < actualTokens.size(); i++) {
+             String actualToken = actualTokens.get(i).getText();
+             System.out.println(actualToken);
+//             assertEquals(actualToken, expectedToken);
+        }
+    }
     // This code is taken from PS2
     public void verifyLexer(String input, String[] expectedTokens) {
         CharStream stream = new ANTLRInputStream(input);
