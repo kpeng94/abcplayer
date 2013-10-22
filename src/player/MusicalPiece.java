e1bd4e88df00727d1b69ea6f4e9415000dc0188d
Kevin Peng <kpeng94@mit.edu>
2013-10-21 21:43:12 -0400

    MusicalPiece.java

--- src/player/MusicalPiece.java
+++ src/player/MusicalPiece.java
@@ -23,8 +23,27 @@ public class MusicalPiece {
      * @return ticks
      */
     public Integer getTicks() {
-        //TODO: (kpeng94) Implement this using phrases
-        return 0;
+        int ticks = 1;
+        int gcd;
+        for (int i = 0; i < phrases.size(); i++) {
+            int phraseTicks = phrases.get(i).getTicks();
+            /**
+             * Computing the gcd of the two will help us compute the LCM.
+             */
+            gcd = 1;
+            if (phraseTicks % 2 == 0 && ticks % 2 == 0)
+                gcd = 2;
+            for (int j = 3; j < Math.min(phraseTicks, ticks); j += 2) {
+                if (phraseTicks % j == 0 && ticks % j == 0) {
+                    gcd = j;
+                }
+            }
+            /**
+             * Compute the least common multiple of ticks and phraseTicks
+             */
+            ticks = phraseTicks * ticks / gcd;
+        }
+        return ticks;
     }
 
     /**
