49c35c2aa2fc8d2566afca071553de9817dc9556
Kevin Peng <kpeng94@mit.edu>
2013-10-21 22:06:02 -0400

    Added Pitch, rest note and bar

---
+++ src/player/RestNote.java
@@ -0,0 +1,38 @@
+package player;
+
+public class RestNote implements Note{
+    private int numerator;
+    private int denominator;
+    private String lyric;
+    
+    public RestNote(int numerator, int denominator, String lyric) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.lyric = lyric;
+    }
+
+    @Override
+    public int getNumerator() {
+        return this.numerator;
+    }
+
+    @Override
+    public int getDenominator() {
+        return this.denominator;
+    }
+
+    @Override
+    public int getNote() {
+        return 0;
+    }
+    
+    @Override
+    public double getLength() {
+        return this.numerator * 1.0 / this.denominator;
+    }
+    
+    @Override
+    public String getLyric() {
+        return this.lyric;
+    }   
+}
